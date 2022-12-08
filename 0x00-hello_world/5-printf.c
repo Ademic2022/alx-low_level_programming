342fc3d95beb7eb7786ef30146212d7ceb0225c9
Ademic <adeeyomichael77@gmail.com>
2022-12-08 17:37:56 +0000

    0x00. C - Hello, World

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
-int main(void){
-	printf("with proper grammar, but the outcome is a piece of art,");
+/**
+ * main - print a C Program using printf
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, /n");
 	return (0);
 }

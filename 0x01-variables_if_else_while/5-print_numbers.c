fe9eac73203619a5045919c5b2c9c53ede572fb0
Ademic <adeeyomichael77@gmail.com>
2022-12-09 14:26:40 +0000

    commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints out all numbers under 10
+ * Return: 0
+ */
+int main(void)
+{
+	int a = 0;
+
+	while (a < 9)
+	{
+		printf("%d\n", a);
+		a++;
+	}
+	return (0);
+}

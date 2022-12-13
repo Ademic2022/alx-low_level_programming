b54740b34f54716225f68ecbcc2f5d201f4fa74f
Ademic <adeeyomichael77@gmail.com>
2022-12-13 15:10:24 +0000

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,6 +1,18 @@
 #include "main.h"
 
 /**
+ * _abs- returns 1 if n is lowercase, 0 otherwise
+ * @n: integer to be tested
+ * Return: abs(n)
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-1 * n);
+}
+/**
  * print_last_digit - pritns the last digit of a number
  * @n: integer whose last digit is to be printed
  * Return: lastdigit of n
@@ -9,6 +21,7 @@ int print_last_digit(int n)
 {
 	int lastDigit;
 
-	lastDigit = (n % 10);
+	lastDigit = _abs((n % 10));
+	_putchar(lastDigit + 48);
 	return (lastDigit);
 }
